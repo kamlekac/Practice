7ad8306dfa4fca64ef1a02a789bc9c5a12ea26db
Chaitanya <chaitanya4974@gmail.com>
2024-02-19 22:56:36 -0800

    made changes

--- src/main/java/Subtraction.java
+++ src/main/java/Subtraction.java
@@ -2,6 +2,7 @@ public class Subtraction {
     public static void main(String[] args) {
         int a=10; int b=8; int c;
         c= a-b;
-        System.out.println(c);
+        System.out.println("The subtraction of the digits is" +c);
+
     }
 }
